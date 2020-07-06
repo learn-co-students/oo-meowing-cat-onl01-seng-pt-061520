b897c3111f536ba9892b2162e0ecfe8e41ba41e9
Idalis Paz <idalispaz@gmail.com>
2020-07-06 02:32:36 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
-## code your solution here. 
+class Cat 
+  
+  attr_accessor :name 
+  
+  def meow=(cat_sound)
+    @meow = cat_sound
+  end 
+  
+  def meow
+    @meow 
+    puts "meow!"
+  end 
+end 
